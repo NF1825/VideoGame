e31995545007790c005f15950a54e1db66660f4c
NF1825 <97575686+NF1825@users.noreply.github.com>
2022-11-06 20:03:58 -0500

    add

---
+++ Test.py
@@ -0,0 +1,19 @@
+import pygame
+import time
+
+class
+
+
+
+
+
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000,600))
+screen.fill((200,200,200))
+
+pygame.display.flip()
+
+time.sleep(5)
